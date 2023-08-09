0c9e5ca7f6ceb742793bd5b22df07def5b3eeb64
Maxmillian Toor <mtoor@microsoft.com>
2023-08-09 08:20:36 -0700

    fixed issue

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 console.log("WORKS!");
 
-function async getPosts()
-{
+const getPosts = async () => {
   let postsuri = "https://raw.githubusercontent.com/xamroot/xamroot.github.io/main/posts.json";
   fetch(postsuri)
-    .then(res=>return res.json())
+    .then(res=>{return res.json()})
     .then(res=>console.log(res));
 }
 
