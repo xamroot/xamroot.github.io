21617925dd54e6befb5bac6328a89ac67bed0d9b
Maxmillian Toor <mtoor@microsoft.com>
2023-08-09 08:32:37 -0700

    added navbar

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 console.log("WORKS!");
-
-const getPosts = async () => {
-  let postsuri = "https://raw.githubusercontent.com/xamroot/xamroot.github.io/main/posts.json";
-  fetch(postsuri)
-    .then(res=>{return res.json()})
-    .then(res=>console.log(res));
-}
-
-getPosts();
